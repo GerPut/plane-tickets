9ae00af05dbe43eb21352ed2aceab96ef6b40bf0
Gerhard <gerhard.putter@yahoo.com>
2021-11-30 11:13:59 +0200

    Added countdown timer function.

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const serviceSelect = document.getElementById('service')
-
 let ticketPrice = +serviceSelect.value
 
 // Update count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
-
     const selectedSeatsCount = selectedSeats.length
-
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice
 }
@@ -21,11 +18,29 @@ serviceSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
     updateSelectedCount()
 })
+
 // Seat click
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected')
-
         updateSelectedCount()
     }
 })
+
+//  Business and Extra space seats Color change
+function addCost() {
+
+}
+
+// countdown
+const startingMinutes = 20;
+let time = startingMinutes * 60;
+const countdown = document.getElementById('countdown');
+setInterval(updateCountdown, 1000)
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    countdown.innerHTML = `${minutes} : ${seconds}`;
+    time--
+}
